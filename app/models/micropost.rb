35b1cb24fa3bbd76330e364ed2af6e26056f5674
chinhnc <chinh0510@gmail.com>
2017-05-21 19:01:15 +0700

    fix upload image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,6 +6,20 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
 
+=begin
+  attr_accessor :image
+  #added for paperclip-drop gem
+  has_attached_file :image,
+                    :storage => :dropbox,
+                    :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
+                    :styles => { :medium => "300x300" , :thumb => "100x100>"},
+                    :dropbox_options => {
+                        :path => proc { |style| "#{style}/#{id}_#{image.original_filename}"},       :unique_filename => true
+                    }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+#  validates :image, :attachment_presence => true
+=end
   private
 
     # Validates the size of an uploaded picture.
